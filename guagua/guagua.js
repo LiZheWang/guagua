93d8173fb3cbbdf0e611673594957a0d6fc0ef79
wanglizhe <515372068@qq.com>
2018-06-20 15:58:07 +0800

    完善

--- guagua/guagua.js
+++ guagua/guagua.js
@@ -16,7 +16,6 @@
     }
     Item.prototype = {
         _init : function(options){
-            this.elem = options.elem ;
             this.ops = options.ops ;
             this.id = options.id ;
             this.config = options.config ;
@@ -25,37 +24,64 @@
             //开始的时间
             this.startTime = null ;
             //持续时间
-            this.duration = this.ops.duration || this.getDuration() ;
-
+            this.duration = 0 ;
             //定时器 ，  用于字幕滚动完成后的回调
             this.timer = null ;
-
-            this._updatePage();
-
-            this.setFontSize( this.ops.fontSize );
-            this.setFontColor( this.ops.fontColor );
-            this.setBackground( this.ops.background );
-            this.setRadiusAndImageSize() ;
-            this.setZindex() ;
-
-
-
+            //弹幕的弹道
+            this.tj = null ;
+            this._createHtml();
             return this ;
         } ,
+
+        _createHtml : function(){
+            var img = "" ;
+            if( this.ops.imgUrl ){
+                img = '<img id="__img" src="'+this.ops.imgUrl+'" >';
+            }
+            this.divid = "gid_" + this.id ;
+            this.elem = document.createElement("div") ;
+            this.elem.id = this.divid ;
+            this.elem.className = "__guaguaBox" ;
+            this.elem.setAttribute("cid" , this.id) ;
+            this.elem.innerHTML = img +
+                '<div class="__con" id="gg_con">'+
+                this.ops.message+
+                '</div>';
+            this.wrapBox = document.body ;
+            if( this.config.elem ) {
+                this.wrapBox = document.querySelector(this.config.elem) ;
+            }
+            if( this.wrapBox ){
+                var first = this.wrapBox.firstChild ;
+                this.wrapBox.insertBefore(this.elem,first);
+            }else{
+                throw new Error("elem没有找到：" + this.elem) ;
+                return null ;
+            }
+            this.img = this.elem.getElementsByTagName("img")[0] ;
+            this.setFontSize( this.ops.fontSize );
+            this.setFontColor( this.ops.fontColor );
+            this.setZindex() ;
+            this.setBackground( this.ops.background );
+            this.setRadiusAndImageSize() ;
+        },
+
         //屏幕宽高
         _updatePage : function(){
             this.winWidth = document.documentElement.scrollWidth ;
             this.winHeight = document.documentElement.scrollHeight ;
             this.itemWidth = this.elem.offsetWidth ;
-            this.itemHeight = this.elem.offsetHeight ;
-            //需要移动的距离
-            this.moveWidth = this.itemWidth * 2 + this.winWidth ;
+            this.itemHeight = this.elem.offsetHeight;
+            if( this.ops.imgUrl ){
+                this.itemWidth = this.itemWidth + this.itemHeight;
+            }
+            this.relaMoveWidth = this.itemWidth * 2 + this.winWidth ;
+            //需要移动的距离 为了保证有一个统一的值，这样滚动速度也才是一致的
+            this.moveWidth = this.winWidth*this.config.durationDouble ;
         },
-
 
         setRadiusAndImageSize:function(){
             var val = this.itemHeight + "px" ;
-            this.img = this.elem.getElementsByTagName("img")[0] ;
             if(this.img){
                 this.img.style.width = val ;
                 this.img.style.height = val;
@@ -71,6 +97,7 @@
 
         setFontSize : function(size){
             this.elem.style.fontSize = size ;
+            this._updatePage();
         },
         setFontColor : function(color){
             this.elem.style.color = color ;
@@ -81,6 +108,14 @@
 
         setPosition : function(){
             this.elem.style.right = -this.itemWidth + "px" ;
+            if( this.tj == null ) return ;
+            var index = this.tj.index;
+            var val = (this.config.lineMarginTop * (index+1)) + this.itemHeight * index ;
+            if( this.ops.position == "bottom" ){
+                this.elem.style.bottom = val + "px" ;
+            }else{
+                this.elem.style.top = val + "px" ;
+            }
         },
         //销毁一个弹幕
         destroy:function(){
@@ -108,13 +143,10 @@
             catchs.splice(index,1) ;
         },
 
-        getDuration:function(){
-            var duration = this.config.durationConfig[this.duration] ;
-            return  duration || this.config.durationConfig['normal'];
+        getDuration:function(type){
+            return  this.config.durationConfig[type || 'slow'];
         },
-
-        show : function(){
-            this._updatePage();
+        _show : function(){
             if( this.ops.specialPosition ){
                 this.elem.classList.add("special") ;
                 this.elem.style.marginLeft = -this.itemWidth/2 + "px" ;
@@ -140,18 +172,21 @@
             }
         },
 
-        //开始滚动
-        start : function(){
+        //开始滚动   tj:弹道对象
+        start : function(tj){
             var _this = this ;
             var index = this.getItemIndex();
-            var _delay = this.ops._delay ;
+            var _delay = this.config._delay ;
             if( index != -1 ) {
                 throw "抱歉id为" + this.id +"的字幕已经存在" ;
                 return false ;
             };
+            //为弹幕设置弹道
+            this.tj = tj ;
+            //基数弹道速度快一点
+            this.duration = this.getDuration((tj.index+1)%2==0 ? 'slow' : 'fast');
             this._clearTimer();
-            this.show();
-
+            this._show();
 
             var specialPosition = this.ops.specialPosition;
             var specialDistance = this.ops.specialDistance ;
@@ -168,9 +203,7 @@
 
                     this.durationValue = this.duration*1000 ;
                 }else{
-
                     this.durationValue = this.ops.specialDuration*1000 ;
-
                     if( specialPosition == 'bottom' ){
                         this.elem.style.bottom = specialDistance + "px" ;
                     }
@@ -184,38 +217,22 @@
 
                 //结束后的回调
                 this.timer = setTimeout(function(){
-                    _this.ops.repet > 1 ? _this.reset() : _this.destroy() ;
+                    _this.destroy() ;
+                    if( _this.ops.repet > 1 ){_this.reset();}
                     if( typeof _this.ops.complete == "function" ){
                         _this.ops.complete();
                     }
-                },this.durationValue );
+                },this.durationValue);
 
                 //缓存对象
                 catchs.push(this);
             }.bind(this),_delay);
-
-
-
-
-
-
-
         },
-
 
         //重新再次执行一遍
         reset : function(){
-            this.removeCatchItem();
-            var repet = this.ops.repet-=1 ;
-            this.ops.repet = repet ;
-            this.hide();
-            this.elem.style.webkitTransition = null ;
-            this.elem.style.transition = null ;
-            this.elem.style.webkitTransform = null ;
-            this.elem.style.transform = null ;
-
-
-            this.start() ;
+            this.ops.repet = this.ops.repet-1 ;
+            this.queue.appendMessage(this.ops) ;
         },
 
     }
@@ -228,22 +245,38 @@
                 elem : '' , //弹幕需要放置到那个元素内？  不传就放body里面
                 zindex : 1000 ,
                 stageHeight : 200 , //舞台的高度，就是弹幕可以弹幕要显示的位置如 top的时候是 0-150 px
-                lines : 5 ,  //显示弹幕的行数
                 lineMarginTop : 10 ,  //弹道之间的间距
-                queueStep : 500 ,
+                dmMarginLeft : 40 , //弹幕之间的距离
+                queueStep : 50 ,  //弹幕队列执行频率
+
+                //滚动的倍数 ，  windowWidth * durationDouble  为了保证滚动距离一致
+                durationDouble : 3 ,
+                _delay : 150 ,  //执行延迟的时间
+
+                //弹幕位置如果为free，则应该动态计算lines
+                lines : 5 ,  //显示弹幕的行数
+                position : 'top' ,   //弹幕位置  top：顶部   bottom:底部   free：全屏任意位置
+                maxFontNumber : 30 ,
                 durationConfig : {
-                    fast : 10 ,
+                    fast : 15 ,
                     slow : 20 ,
-                    normal : 15 ,
                 },
             },options);
 
 
 
+            this.ops.durationConfig.fast = this.ops.durationConfig.fast * this.ops.durationDouble ;
+            this.ops.durationConfig.slow = this.ops.durationConfig.slow * this.ops.durationDouble ;
+
+            //队列执行的间隔
+            this.queueStep = this.ops.queueStep ;
+
             //弹道队列，每一行数据代表一个弹道，每个弹道里面存放最后一条弹幕
             this.tjs = [] ;
-            //this._createTrajectory();
+            //弹幕队列
+            this.dms = [] ;
 
+            this._createTrajectory();
             this._runTrajectoryQueue();
 
             return this ;
@@ -255,6 +288,7 @@
                 for( var i=0;i<this.ops.lines;i++ ){
                     this.tjs.push({
                         name : "d" + (i+1),
+                        index : i ,
                         //top : (this.ops.lineMarginTop * (i+1)) + this.itemHeight * i ,
                         items : [] ,   //弹道内的弹幕
                     });
@@ -266,14 +300,32 @@
         _getTrajectory : function(){
             //如果弹道内没有一个弹幕，优先塞入弹幕(优先在空闲的弹道塞入弹幕)
             var find = this.tjs.find(function(item){ return item.items.length == []  }) ;
-            if( find ) return find ;
-            //如果没有空闲的弹道，
+            if( find  ) return find ;
+            //如果弹道内都有弹幕，则查看最后一个弹幕有没有完全显示出来，如果都显示出来了，就可以继续塞入弹幕
+            var result = null ;
+            this.tjs.forEach((item,index) => {
+                var items = item.items ;
+                var last = items[items.length-1] ;
+                //获取弹幕滚动了多久(获取移动时间)
+                var difftime = new Date().getTime() - last.startTime ;
+                //弹幕每微秒移动的距离
+                var s_val = last.moveWidth / last.durationValue ;
+                //每微秒速度 * 滚动时间 >=  弹幕的长度 + 弹幕间距 。  那么说明弹幕已经全部显示了，队列中可以添加新弹幕了
+                if( s_val * difftime  >= last.itemWidth + this.ops.dmMarginLeft ){
+                    result = item ;
+                }
+            });
+            return result ;
         },
 
         //往弹道队列中塞入一条弹幕  item：弹幕
         _addTrajectory : function(item){
             var tj = this._getTrajectory() ;
-            tj.items.push(item);
+            if( tj != null ){
+                tj.items.push(item);
+                this._delDm(item.id);
+                item.start(tj);
+            }
         },
         //删除一个弹道队列中的弹幕（弹幕显示完毕后，需要从弹道中删除）, 传入弹幕id
         _delTrajectory : function(id){
@@ -286,11 +338,29 @@
         },
         //执行弹道队列
         _runTrajectoryQueue : function(){
-            setTimeout(() => {this._runTrajectoryQueue();},this.ops.queueStep);
+            if( this.dms.length ){
+                this._addTrajectory(this.dms[0]);
+            }
+            setTimeout(() => {this._runTrajectoryQueue();},this.queueStep);
         },
 
-        //创建弹幕
-        _createDmItem(options){
+        //弹幕队列 添加
+        _addDm : function(item){
+            if(item){
+                this.dms.push(item)
+            }else{
+                throw("添加弹幕失败:" + item);
+            }
+        },
+        //弹幕队列 删除
+        _delDm : function(id){
+            var findIndex = this.dms.findIndex(item => item.id == id) ;
+            if( findIndex != -1 ) this.dms.splice(findIndex,1) ;
+        },
+        //弹幕队列 获取要执行的弹幕
+        _getCurrentDm : function(){return this.dms[0] ;},
+
+        appendMessage : function(options){
             if( !options.message ) {
                 throw new Error("弹幕内容(content)字段不能为空") ;
                 return false ;
@@ -318,66 +388,26 @@
                 //特殊字幕的显示时间
                 specialDuration : 4 ,
 
-                _delay : 150 ,  //执行延迟的时间
 
                 fontSize : "14px" ,
                 fontColor : '#fff' ,
                 delay: 0 , //是否延迟？
                 repet : 1 , // 重复的次数
 
-                //direction : 'right' ,  //left:从左到右   right：从右到左 。 暂不实现一般都是右向左
-                position : 'top' ,   //top：顶部   bottom:底部   center：中间 ， free：全屏任意位置
                 imgUrl:"" ,
             },options||{});
 
-            //if( params.fontSize == "small" )  params.fontSize = "12px" ;
-            //if( params.fontSize == "big" )  params.fontSize = "16px" ;
-
-
-            var img = "" ;
-            if( params.imgUrl ){img = '<img id="__img" src="'+params.imgUrl+'" >';}
-
-
-            var item = document.createElement("div") ;
-            item.className = "__guaguaBox" ;
-            item.id = "__guaguaBox" ;
-            item.setAttribute("cid" , options.id) ;
-            item.innerHTML = img +
-                            '<div class="__con">'+
-                            options.message+
-                            '</div>';
-
-            this.wrapBox = document.body ;
-            if( this.ops.elem ) {
-                this.wrapBox = document.querySelector(this.ops.elem) ;
+            var itemParams = {
+                id : params.id ,
+                ops : params ,
+                config : this.ops ,
+                //guagua实例对象
+                queue : this
             }
-
-            if( this.wrapBox ){
-                var first = this.wrapBox.firstChild ;
-                this.wrapBox.insertBefore(item,first);
-                var itemParams = {
-                    id : params.id ,
-                    elem:item,
-                    ops : params ,
-                    config : this.ops ,
-                    //guagua实例对象
-                    queue : this
-                }
-                return new Item(itemParams);
-            }else{
-                throw new Error("elem没有找到：" + this.ops.elem) ;
-                return null ;
-            }
-
-        } ,
-
-        appendMessage : function(options){
-            this._addTrajectory(this._createDmItem(options));
+            var item =  new Item(itemParams);
+            this._addDm(item);
+            return item ;
         }
-
-
-
-
 
 
 
